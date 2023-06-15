8082cbcb6ff1fa497e625aeaf10548ef01e3bf7d
Akash1590 <ar156403@gmail.com>
2023-06-15 05:07:33 +0200

    commit message

--- index.py
+++ index.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, jsonify, send_file
 import os
 
-# from amazon_credentials import access_key_id, secret_access_key, Region
+# We are not sharing our amazon credentials to git repository. That's why user's need to rename the file name. Here
+# check if the file "amazon_credentials.py" exists and import the credentials if it does otherwise import the default
 if os.path.isfile("amazon_credentials.py"):
     from amazon_credentials import access_key_id, secret_access_key, Region
 else:
@@ -15,19 +16,24 @@ import datetime
 import requests
 import boto3
 
-# from botocore.exceptions import BotoCoreError, NoCredentialsError
-
 app = Flask(__name__)
 
+# Open Meteo Default API URL
 API_URL = "https://api.open-meteo.com/v1/forecast"
+# Location coordinates of Cologne City
 LATITUDE = 50.93
 LONGITUDE = 6.95
-INTERVAL = 24*60*60  # Interval in seconds (e.g., 3600 for 1 hour)
+# Default Interval Values. Here We set 60 secs by default
+INTERVAL = 30*60  # Interval in seconds (e.g., 3600 for 1 hour)
+# Flag indicating if Amazon AWS credentials are available
 amazon_flag = False
+
+# Create an S3 Client using the credentials
 s3 = boto3.client('s3', aws_access_key_id=access_key_id,
                   aws_secret_access_key=secret_access_key,
                   region_name=Region)
 
+# Here We are testing the S3 connection if the credentials are authentic or not.
 try:
     response = s3.list_buckets()
     print("Connection to S3 successful. Buckets:")
@@ -40,10 +46,9 @@ except Exception as e:
     print("An error occurred:", str(e))
 
 
+# Function to retrieve current weather data from Open-Meteo API
 def get_current_weather_data():
     print("INTERVAL:" + str(INTERVAL))
-    # url = f"{API_URL}?latitude={LATITUDE}&longitude={LONGITUDE}&current_weather=true&hourly=rain,showers,
-    # visibility," \ f"temperature_2m&timezone=Europe/Berlin&forecast_days=1"
     url = f"{API_URL}?latitude={LATITUDE}&longitude={LONGITUDE}"
     params = {
         "current_weather": "true",
@@ -61,6 +66,7 @@ def get_current_weather_data():
         return None
 
 
+# Function to write weather data to a CSV File
 def write_to_csv(data):
     if data:
         current_timestamp = time.time()
@@ -95,6 +101,7 @@ def write_to_csv(data):
         print("No weather data available to write to CSV.")
 
 
+# Function to continusosly fetch weather data in a separate Thread
 def fetch_weather_data():
     global weather_data
     global INTERVAL
@@ -121,16 +128,22 @@ def fetch_weather_data():
             current_weather_data['last_update_time'] = formatted_datetime
 
             weather_data = current_weather_data
+            print(weather_data)
         time.sleep(INTERVAL)
 
 
+# Route for the home page with the current weather data. If there is no weather data it will return appropriate
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # print(weather_data)
-    print("INTERVAL" + str(INTERVAL))
-    return render_template('index.html', weather_data=weather_data)
+    try:
+        print("INTERVAL" + str(INTERVAL))
+        return render_template('index.html', weather_data=weather_data)
+    except NameError:
+        print("Hello")
+        return render_template('index.html')
 
 
+# Route for updating the data fetch Interval. It will update the global variable Interval.
 @app.route('/update_interval', methods=['GET'])
 def update_interval():
     global INTERVAL  # Access the global variable
@@ -138,10 +151,12 @@ def update_interval():
     interval_minutes = int(request.args.get('intervalMinutes'))
     INTERVAL = (interval_hour * 60 + interval_minutes) * 60  # Convert hours and minutes to seconds
     print("INTERVAL" + str(INTERVAL))
-    interval_msg= "Update Interval time will be : "+str(interval_hour)+" hours and "+ str(interval_minutes)+" minutes";
+    interval_msg = "Update Interval time will be : " + str(interval_hour) + " hours and " + str(
+        interval_minutes) + " minutes";
     return jsonify({'status': interval_msg})
 
 
+# Route for getting a list of all files in the 'csv' folder
 @app.route('/get_allfiles', methods=['GET'])
 def get_allfiles():
     try:
@@ -156,6 +171,7 @@ def get_allfiles():
     return jsonify(response), status_code
 
 
+# Route for downloading the csv file
 @app.route('/download', methods=['GET'])
 def download():
     fileName = request.args.get('filename')
@@ -164,6 +180,7 @@ def download():
     # return jsonify({'status': 'success'})
 
 
+# Route for deleteing the a file from the 'csv' Folder
 @app.route('/delete', methods=['GET'])
 def delete():
     fileName = request.args.get('filename')
@@ -182,12 +199,15 @@ def delete():
     return jsonify(response), status_code
 
 
+# Main Entry point of the application
 if __name__ == '__main__':
     if not os.path.exists('csv'):
         os.makedirs('csv')
     else:
         print("Folder Already Exists")
+    # Start a separate thread for fetching weather data
     weather_thread = threading.Thread(target=fetch_weather_data)
     weather_thread.daemon = True
     weather_thread.start()
+    # Run the Flask application on port 5000
     app.run(host='0.0.0.0', port=5000)
